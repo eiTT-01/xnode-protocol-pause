94e8f74a068bd3437c781f9423209b1c376898bf
eiTT-01 <xavier-leroy@outlook.com>
2025-05-29 13:20:08 +0800

    fix dependence error

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -5,9 +5,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className='min-h-screen bg-gray-50'>
             <div className='container mx-auto px-4 py-4 max-w-4xl'>
                 <div className='flex justify-end mb-4'><Menu /></div>
-                <div className='flex justify-center'>
+            <div className='flex justify-center'>
                     <div className='w-full'>
-                        {children}
+                {children}
                     </div>
                 </div>
             </div>
